26e5d44f59ce5607a0defad1eaba50213ca8e338
Bruno Alex Oliveira <brunoal.flat@gmail.com>
2022-09-20 15:02:14 -0300

    Finalizado o teste.

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
 # Começo
-print('Olá, estamos no nosso primeiro projeto!')
+nome = input('Qual seu nome?')
+idade = input('Qual a sua idade?')
+dinheiro = input('Quanto você tem na sua conta bancária?')
+
+print(f'Parabéns {nome}! Por ter a idade de {idade} e por ter {dinheiro} na sua conta bancária, você merece simplesmente um depoimento no orkut falando como você é especial pakas. Obrigado.')
